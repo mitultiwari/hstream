d81449cb66b46eb586fc304f29d2b7fed4e1a64a
Kartik K. Agaram <vc@akkartik.com>
2011-02-20 20:44:54 -0800

    ah, this makes more sense

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,22 +1,2 @@
 Factory.define :item do |i|
-  i.hnid 10
-  i.association :parent, :factory => :parent
-  i.contents "item"
-end
-
-Factory.define :parent, :class => 'item' do |i|
-  i.hnid 6
-  i.association :parent, :factory => :grandparent
-  i.contents "parent"
-end
-
-Factory.define :grandparent, :class => 'item' do |i|
-  i.hnid 2
-  i.contents "frist post!!1"
-end
-
-Factory.define :uncle, :class => 'item' do |i|
-  i.hnid 5
-  i.association :parent, :factory => :grandparent
-  i.contents "uncle"
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Item do
   before(:each) do
-    @grandparent = Factory(:grandparent)
-    @uncle = Factory(:uncle)
-    @parent = Factory(:parent)
-    @item = Factory(:item)
+    @parent = Factory :item, :hnid => 2
+    Factory :item, :hnid => 5, :parent => @parent
+    @item = Factory :item, :hnid => 6, :parent => @parent
+    @child = Factory :item, :hnid => 10, :parent => @item
   end
 
   describe :levels_below do
@@ -16,16 +16,16 @@ describe Item do
       @parent.levels_below(@item).should == 1
     end
     it "should work with grandchildren" do
-      @grandparent.levels_below(@item).should == 2
+      @parent.levels_below(@child).should == 2
     end
   end
 
   describe :levels_of_descendants do
     it "should work on leaves" do
-      @item.levels_of_descendants.should == 0
+      @child.levels_of_descendants.should == 0
     end
     it "should work on internals" do
-      @grandparent.levels_of_descendants.should == 2
+      @parent.levels_of_descendants.should == 2
     end
   end
 end
