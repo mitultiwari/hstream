5d81af167c479aeeb2c18b5b5ab57aed20710228
Kartik K. Agaram <vc@akkartik.com>
2011-02-21 13:19:54 -0800

    also don't reorder shortlist in the controller

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -9,7 +9,8 @@ class RootController < ApplicationController
       shortlist_ids = params[:shortlist].split(',').collect{|x| x.to_i}
       if !params[:refreshShortlist].blank? ||
             @recentItems.index{|x| shortlist_ids & x.ancestors.collect(&:hnid) != []}
-        @shortlist = Item.find(:all, :conditions => ['hnid in (?)', shortlist_ids])
+        @shortlist = Item.find(:all, :conditions => ['hnid in (?)', shortlist_ids]).
+                          sort_by{|x| shortlist_ids.index(x.hnid)}
       end
     end
   end
