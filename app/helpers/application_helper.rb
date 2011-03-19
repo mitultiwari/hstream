d362e9c7db3d22274d1e385b5bcd8a7c8d90a03d
Kartik K. Agaram <vc@akkartik.com>
2011-03-19 01:47:31 -0700

    remove duplicated code. but is this layer of abstraction an improvement? I don't like the names jsrender and htmlrender. I don't like having to say raw(htmlrender).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def htmlrender(id, partial, options)
+    "<div id=\"#{id.to_s}\">#{render partial.to_s, options}</div>"
+  end
+  def jsrender(id, partial, options)
+    "$('##{id.to_s}').prepend(\"#{escape_javascript(render partial.to_s, options)}\");"
+  end
+
   def layout
     @@layout ||= ActiveSupport::JSON.decode File.open("#{RAILS_ROOT}/config/app.json")
   rescue
