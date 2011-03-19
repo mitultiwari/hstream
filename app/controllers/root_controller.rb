8683646e43ccea19fea6e2d06fb9d1fa24861e64
Kartik K. Agaram <vc@akkartik.com>
2011-03-19 01:52:14 -0700

    standardize naming: recentItems => stream

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,7 @@
 class RootController < ApplicationController
   def index
-    @recentItems = Item.since(params[:mostRecentItem])
-    @mostRecentItem = @recentItems[0].hnid rescue params[:mostRecentItem]
-    @shortlist = Item.shortlist_children(@recentItems, params[:shortlist])
+    @stream = Item.since(params[:mostRecentItem])
+    @shortlist = Item.shortlist_children(@stream, params[:shortlist])
+    @mostRecentItem = @stream[0].hnid rescue params[:mostRecentItem]
   end
 end
