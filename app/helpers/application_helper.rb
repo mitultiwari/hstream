6b5115c044a25b4b258f4e2986ad44c59e30a81a
Kartik K. Agaram <vc@akkartik.com>
2011-03-21 23:00:14 -0700

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   end
 
   def layout
-    @@layout ||= ActiveSupport::JSON.decode File.open("#{RAILS_ROOT}/config/app.json")
+    @@layout ||= ActiveSupport::JSON.decode File.open("#{Rails.root}/config/app.json")
   rescue
     @@layout ||= { 'root' => 'http://news.ycombinator.com',
                    'title' => 'Hacker News in real-time',
