1918aa6fedfa5cdce9224b300cf205302b5f931d
Kartik K. Agaram <vc@akkartik.com>
2011-04-01 05:18:04 -0700

    start logging referer info
    Too much trouble to mess with the existing 'Started' line, just make your own.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter lambda {
+    logger.debug "STAT: #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}"
+  }
 end
