3f7e14edb52e55c00aaf84cff74694b7a5845050
Kartik K. Agaram <vc@akkartik.com>
2011-04-03 19:59:39 -0700

    show most recent matching items even if they aren't fresh

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -1,6 +1,6 @@
 class StoryController < ApplicationController
   def show
-    @items = {:stream => Item.since(params[:mostRecentItem])}
+    @items = {:stream => Item.where('story_hnid = ?', params[:id]).since(params[:mostRecentItem])}
     @mostRecentItem = @items[:stream][0].hnid rescue params[:mostRecentItem]
     @items[:stream] = @items[:stream].select{|x| x.story_hnid == params[:id].to_i}
     @items[:shortlist] = []
