5ba97fcdf09df847753b82b3f466089d4e9f8149
Kartik K. Agaram <vc@akkartik.com>
2011-04-03 20:14:57 -0700

    simplify item.since, and make order('id desc') a default scope

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :parent, :class_name => 'Item', :foreign_key => 'parent_hnid', :primary_key => 'hnid'
   belongs_to :story, :class_name => 'Item', :foreign_key => 'story_hnid', :primary_key => 'hnid'
-  scope :since, lambda {|hnid| since_scope(hnid)}
+  default_scope order('id desc')
 
   def ancestors
     ancestors = []
@@ -23,9 +23,8 @@ class Item < ActiveRecord::Base
     new.select{|x| shortlist.index(x.parent_hnid) || shortlist.index(x.author)}
   end
 
-  def self.since_scope(hnid)
-    scope = scoped.order('id desc')
-    return scope.limit(20) if [nil, '', '0', 0].index(hnid)
-    return scope.where('id > ?', Item.find_by_hnid(hnid).id).limit(10)
+  def self.since(hnid)
+    return limit(20) if [nil, '', '0', 0].index(hnid)
+    return where('id > ?', Item.find_by_hnid(hnid).id).limit(10)
   end
 end
