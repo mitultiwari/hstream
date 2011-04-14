9a5aa5180d99dffd4d72f4e543f73b1a9b0629d7
Kartik K. Agaram <vc@akkartik.com>
2011-04-14 12:14:41 -0700

    retesting with an updating stream

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -1,8 +1,8 @@
 class StoryController < ApplicationController
   def show
     @mostRecentItem, @items = initialize_item_scopes(params)
-    @items[:story] = @items[:stream].where('story_hnid = ?', params[:id])
     @id = 'story_'+params[:id]
+    @items[@id] = @items[:stream].where('story_hnid = ?', params[:id])
     @title = Item.title(params[:id])
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,8 @@
 class UserController < ApplicationController
   def show
     @mostRecentItem, @items = initialize_item_scopes(params)
-    @items[:user] = @items[:stream].where('author = ?', params[:id])
     @user = params[:id]
     @id = 'user_'+@user
+    @items[@id] = @items[:stream].where('author = ?', params[:id])
   end
 end
