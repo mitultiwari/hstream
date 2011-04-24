b445422e1414eee4018174c173a42b9ab959b45b
HackerStream <feedback@readwarp.com>
2011-04-23 20:36:57 -0700

    rendering views uncovered sloppiness in my handling of timestamps

--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -1,11 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RootController do
+  render_views
+
   describe "#index" do
     it "should populate @items['stream'] on initial load" do
-      Factory(:item, :hnid => 1)
-      Factory(:item, :hnid => 2)
-      Factory(:item, :hnid => 3)
+      Factory(:item, :hnid => 1, :timestamp => 3.seconds.ago)
+      Factory(:item, :hnid => 2, :timestamp => 2.seconds.ago)
+      Factory(:item, :hnid => 3, :timestamp => 1.second.ago)
       get :index
       assigns(:items)['stream'].map(&:hnid).should == [3, 2, 1]
     end
