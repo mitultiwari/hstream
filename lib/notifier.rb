fbb4a9320ce9fe4f73ef5646116ac842c652b2c8
HackerStream <feedback@readwarp.com>
2011-04-25 21:59:28 -0700

    testable organization of notifier script

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,1 +1,18 @@
-NotificationMailer.pattern_email('akkartik@gmail.com', 'akkartik', '64').deliver
+module Notifier
+  def sendEmailsFor(item)
+    NotificationMailer.pattern_email('akkartik@gmail.com', 'akkartik', '64').deliver
+  end
+
+  def run
+    mostRecentId = Item.first.id
+    while true
+      sleep 30
+      puts Time.now
+      curr = Item.first.id
+      Item.where('id > ? and id <= ?', mostRecentId, curr).each do |item|
+        sendEmailsFor(item)
+      end
+      mostRecentId = curr
+    end
+  end
+end
--- lib/tasks/notifier.rake
+++ lib/tasks/notifier.rake
@@ -2,6 +2,7 @@ namespace :notifier do
   desc "monitor new items as they get crawled and send out email notifications"
   task :run => :environment do
     require 'notifier'
+    Notifier.run
   end
 
 end
