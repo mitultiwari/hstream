05314c2d05371b0e2ff7dcad21e17cf42ed7293d
HackerStream <feedback@readwarp.com>
2011-05-11 22:43:24 -0700

    simpler way to merge on login

--- app/models/login.rb
+++ app/models/login.rb
@@ -3,4 +3,14 @@ class Login < ActiveRecord::Base
     self[:shortlist] = (shortlist.split(',')+s.split(',')).uniq.join(',')
     save
   end
+
+  def self.create_or_merge(email, shortlist)
+    user = find_by_email(email)
+    if user
+      user.merge_shortlist(shortlist)
+    else
+      user = Login.create(:email => email, :shortlist => shortlist)
+    end
+    user
+  end
 end
--- config/initializers/active_record.rb
+++
@@ -1,7 +0,0 @@
-module ActiveRecord
-  class Base
-    def self.find_or_create(key, params)
-      where("#{key} = ?", params[key]).first || create(params)
-    end
-  end
-end
