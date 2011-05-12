15e4b53cf402a4b430774743dbe8883b98fa6f68
HackerStream <feedback@readwarp.com>
2011-05-12 15:01:45 -0700

    logout

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,4 +3,10 @@ class LoginsController < ApplicationController
     session[:user] = Login.create_or_merge(params[:email], params[:shortlist])
     cookies['email'] = session[:user].email
   end
+
+  def destroy
+    session[:user] = nil
+    cookies['email'] = nil
+    render :text => ''
+  end
 end
