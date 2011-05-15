f2cd0f945ace5b09e109bf40b4ab0ca075ad6a76
HackerStream <feedback@readwarp.com>
2011-05-14 21:15:03 -0700

    include the new login information in our logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter lambda {
-    logger.debug "STAT: #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}"
-  }
-  before_filter lambda {
     cookies['email'] = session[:user].email if session[:user]
+    logger.debug "STAT: #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}#{currcookie}"
   }
 
   def initialize_item_scopes(params)
     bound = params[:until] ? Item.find_by_hnid(params[:until]) : Item.first
     return [bound.hnid, {'stream' => Item.since(params[:mostRecentItem], bound.id)}]
+  end
+
+  def currcookie
+    return '' if cookies['email'].blank?
+    ' ('+cookies['email']+')'
   end
 end
