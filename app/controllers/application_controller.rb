ffa416c29baff066cc384462cab5adddd6296dec
HackerStream <feedback@readwarp.com>
2011-05-25 00:04:59 -0700

    include session cookie in STAT logline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter lambda {
     cookies['email'] = session[:user].email if session[:user]
-    logger.debug "STAT: #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}#{currcookie}"
+    logger.debug "STAT: #{request.session_options[:id]} #{cookies['email'].inspect} #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}"
   }
 
   def initialize_item_scopes(params)
     bound = params[:until] ? Item.find_by_hnid(params[:until]) : Item.first
     return [bound.hnid, {'stream' => Item.since(params[:mostRecentItem], bound.id)}]
-  end
-
-  def currcookie
-    return '' if cookies['email'].blank?
-    ' ('+cookies['email']+')'
   end
 end
