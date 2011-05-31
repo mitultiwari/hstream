56005de59f71a35411161e9c2bc800bd36045be2
HackerStream <feedback@readwarp.com>
2011-05-31 00:33:01 -0700

    purge obsolete code for arc forum config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,13 +9,4 @@ module ApplicationHelper
     raw "$('#'+jqEsc('#{id}')+' .holding').prepend(\"#{escape_javascript(render 'root/show_items', :items => @items[id])}\");
     $('#'+jqEsc('#{id}')+' .holding .item:gt('+maxColumnCapacity+')').remove();"
   end
-
-  def layout
-    @@layout ||= ActiveSupport::JSON.decode File.open("#{Rails.root}/config/app.json")
-  rescue
-    @@layout ||= { 'root' => 'http://news.ycombinator.com',
-                   'title' => 'Hacker News in real-time',
-                   'topcolor' => '#ff6600',
-                 }
-  end
 end
