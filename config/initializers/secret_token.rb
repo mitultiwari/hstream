c2801223ed5773e8faa36c103ae2c811ceb7367f
HackerStream <feedback@readwarp.com>
2011-06-02 15:44:42 -0700

    switch sessions to the server rather than the client cookiestore.
    Now we'll have to watch for this table growing too large.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hnrt::Application.config.secret_token = 'cc713faafc9ce865d7382290de1496722deae2b08fb24de8feccac5e88ce0b0f4843c3739a4aaa6f76bb37724e0edda20175cb9e8b015715a5bc71d545ed849b'
