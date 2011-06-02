7fdf84b8567754d013cf9770a49f7e987d21c3eb
HackerStream <feedback@readwarp.com>
2011-06-02 16:36:17 -0700

    email wasn't rendering right, on the nav or in logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter lambda {
-    cookies['email'] = session[:user].email if session[:user]
-    logger.debug "STAT: #{request.session_options[:id] || 0} #{cookies['email'] || 'nil'} #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}"
+    cookies['email'] = session[:user].email.email if session[:user]
+    logger.debug "STAT: #{request.session_options[:id] || 0} #{session_email} #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}"
   }
 
   def initialize_item_scopes(params)
     bound = params[:until] ? Item.find_by_hnid(params[:until]) : Item.first
     return [bound.hnid, {'stream' => Item.since(params[:mostRecentItem], bound.id)}]
+  end
+
+  def session_email
+    return 'nil' unless session[:user]
+    return 'nil' if session[:user].email.email.blank?
+    session[:user].email.email
   end
 end
