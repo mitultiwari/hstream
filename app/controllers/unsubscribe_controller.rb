92335cec58a2026cdba4dc4a8550e2d92352f91c
HackerStream <feedback@readwarp.com>
2011-06-04 11:13:47 -0700

    404 for reusing subscriptions

--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -1,6 +1,7 @@
 class UnsubscribeController < ApplicationController
   def show # can't use delete; will be clicked on from emails
     @subscription = Subscription.find_by_code(params[:id])
+    return render(:status => 404, :file => "#{Rails.root}/public/404.html") unless @subscription
     @subscription.destroy
     render :layout => false
   end
