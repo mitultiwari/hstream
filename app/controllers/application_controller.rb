e57669bbc5114f9079c395b9cf5fc23a11286281
HackerStream <feedback@readwarp.com>
2011-06-14 18:49:46 -0700

    don't lose the session id in the first request of a session. Why was this so hard to think of?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter lambda {
+  after_filter lambda {
     cookies['email'] = session[:user].email.email if session[:user]
     logger.debug "STAT: #{request.session_options[:id] || 0} #{session_email} #{request.ip} #{request.remote_ip} requested #{request.path}?#{request.query_string} via #{request.referer}"
   }
